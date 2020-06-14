481b8186b1be6be5ecf2a3a6e9e097c810243bc1
Dirosaki <diegosgomes27@gmail.com>
2020-06-14 05:03:27 -0300

    pwabuilder

--- js/app.js
+++ js/app.js
@@ -1,30 +1,8 @@
-      // CODELAB: Add code to call getForecastFromCache.
-      getForecastFromCache(location.geo)
-      .then((forecast) => {
-        renderForecast(card, forecast);
-      });
+// This is the "Offline copy of pages" service worker
 
-// If the data on the element is newer, skip the update.
-if (lastUpdated >= data.currently.time) {
-    return;
-  }
+// Add this below content to your HTML page inside a <script type="module"></script> tag, or add the js file to your page at the very top to register service worker
 
-// CODELAB: Add code to get weather forecast from the caches object.
-if (!('caches' in window)) {
-    return null;
-  }
-  const url = `${window.location.origin}/forecast/${coords}`;
-  return caches.match(url)
-      .then((response) => {
-        if (response) {
-          return response.json();
-        }
-        return null;
-      })
-      .catch((err) => {
-        console.error('Error getting data from cache', err);
-        return null;
-      });
+import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
 
-
-
+const el = document.createElement('pwa-update');
+document.body.appendChild(el);
